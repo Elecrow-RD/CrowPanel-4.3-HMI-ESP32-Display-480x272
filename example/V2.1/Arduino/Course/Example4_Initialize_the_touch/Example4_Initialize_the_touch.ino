e2eaa5963bb4d99311dabbb6763e69e4aebf2367
Elecrow <24800915+Elecrow-RD@users.noreply.github.com>
2026-06-16 10:01:33 +0800

    V2.1

---
+++ example/V2.1/Arduino/Course/Example4_Initialize_the_touch/Example4_Initialize_the_touch.ino
@@ -0,0 +1,22 @@
+#include "touch.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin( 115200 ); 
+  touch_init();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (touch_has_signal())
+  {
+    if (touch_touched())
+    {
+      Serial.print( "Data x :" );
+      Serial.println( touch_last_x );
+
+      Serial.print( "Data y :" );
+      Serial.println( touch_last_y );
+    }
+  }
+}
